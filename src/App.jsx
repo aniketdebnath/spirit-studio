5a074214838725e86db8b8314a10dcce975ae76a
aniketdebnath <rkstaraniket34@gmail.com>
2025-02-05 13:02:24 +1100

    starter,pages

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <h2>home page</h2>,
+    },
+    {
+      path: "/about",
+      element: (
+        <div>
+          <h2>about page</h2>
+        </div>
+      ),
+    },
+  ]);
+  return <RouterProvider router={router} />;
+};
+export default App;
